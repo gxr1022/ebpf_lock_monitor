d794eb168da7f550a15d8a14448bf571c7f88068
gxr1022 <1963240214@qq.com>
2024-07-10 21:02:39 +0800

    initial commit

--- scripts/run_mongo_lock_count_analysis.sh
+++ scripts/run_mongo_lock_count_analysis.sh
@@ -2,29 +2,34 @@
 # set -x
 perf_time=$1
 process_id=$2
+comm_name=$3
 PREFIX_PATH="/home/gxr/mongodb-run/ebpf_monitor"
 current=`date "+%Y-%m-%d-%H-%M-%S"`
 cmd_path="$PREFIX_PATH/src/mongo_lock_count_analysis.py"
 
-tid_output=$(ps -T -p $process_id) 
+# tid_output=$(ps -T -p $process_id) 
+# echo "$tid_output"
+# comm_array=($(echo "$tid_output" | awk 'NR>1 {print $5}'))
 
-echo "$tid_output"
-
-comm_array=($(echo "$tid_output" | awk 'NR>1 {print $5}'))
-
-OUTPUT_PATH=${PREFIX_PATH}/locks_analysis_log/${current}.analysis
+OUTPUT_PATH=${PREFIX_PATH}/locks_analysis_log/${current}.analysis.${process_id}
 
 mkdir -p $OUTPUT_PATH
 
-len=${#comm_array[@]}
+output_file=${OUTPUT_PATH}/trace_locks.${process_id}.${comm_name}.log
+sudo python $cmd_path  $comm_name  2>&1 | tee -a ${output_file}
 
-for ((i=47; i<len; i++)); do
-        comm_name="${comm_array[i]}"
-        output_file=${OUTPUT_PATH}/trace_locks.${perf_time}.${process_id}.${comm_name}.log
-        echo "$output_file"
-        echo "$cmd_path"
-        sudo python $cmd_path  $comm_name  $perf_time  2>&1 | tee -a ${output_file}
-done
+
+# len=${#comm_array[@]}
+
+# for ((i=0; i<40; i++)); do
+#         if [[ "$comm_name" == *"conn"* ]]; then
+#                 comm_name="${comm_array[i]}"
+#                 output_file=${OUTPUT_PATH}/trace_locks.${process_id}.${comm_name}.log
+#                 echo "$output_file"
+#                 echo "$cmd_path"
+#                 sudo python $cmd_path  $comm_name  2>&1 | tee -a ${output_file}
+#         fi
+# done
 
 
 
